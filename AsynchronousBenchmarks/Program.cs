51de56e2dc32b1e3ab3195622dfee044400dc926
Adam Sitnik <adam.sitnik@gmail.com>
2020-11-06 11:22:17 +0100

    tell BDN to pass "/p:Progress=false" MsBuild property when building projects
    use BenchmarkSwitcher instead of BenchmarkRunner

--- AsynchronousBenchmarks/Program.cs
+++ AsynchronousBenchmarks/Program.cs
@@ -1,9 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-
-[assembly: SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput, BenchmarkDotNet.Jobs.RuntimeMoniker.Mono)]
-[assembly: SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput, BenchmarkDotNet.Jobs.RuntimeMoniker.CoreRt31)]
-[assembly: SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput, BenchmarkDotNet.Jobs.RuntimeMoniker.Net48)]
 
 namespace AsynchronousBenchmarks
 {
@@ -12,11 +10,33 @@ namespace AsynchronousBenchmarks
         public static readonly object obj = new object();
         public static readonly Vector4 vector = new Vector4(1f, -2.1f, -4f, 5.5f);
 
+        /// <summary>
+        /// to run specific benchmarks you must pass `--filter $glob`. Examples:
+        ///     * --filter '*' // runs all
+        ///     * --filter 'ContinuationBenchmarks' // runs only ContinuationBenchmarks
+        /// to run against specific runtimes you need to pass their target framework monikers using --runtimes. Examples:
+        ///     * --runtimes net472 mono netcoreapp31 corert31
+        /// to disable "Progress" modify the .csproj file and set it to false
+        /// Sample full command:
+        ///     * dotnet run -c Release -f net472 --filter * --runtimes net472 mono netcoreapp31 corert31
+        /// For more info please refer to https://benchmarkdotnet.org/articles/configs/toolchains.html#multiple-frameworks-support
+        /// </summary>
         public static void Main(string[] args)
         {
-            BenchmarkRunner.Run<ContinuationBenchmarks>();
-            BenchmarkRunner.Run<AwaitBenchmarks>();
-            BenchmarkRunner.Run<AsyncBenchmarks>();
+            // you can change it to Job.Default but Short just takes less time to run and is good for testing
+            var job = Job.ShortRun;
+
+#if PROTO_PROMISE_PROGRESS_DISABLE
+            // define custom MsBuildArgument that BenchmarkDotNet MUST pass to MsBuild when building the auto-generated projects
+            // and the referenced proejcts
+            job = job.WithArguments(new Argument[] { new MsBuildArgument("/p:Progress=false") });
+#endif
+            // tell BDN that this job should be extended by arguments passed from command line
+            job = job.AsDefault();
+
+            var config = DefaultConfig.Instance.AddJob(job);
+
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
         }
     }
 
